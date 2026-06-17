317db73a2b5ea7691e37c6bb54560ab5718c07db
HeRa Khan <heraemt@gmail.com>
2026-06-17 14:25:12 +0600

    1

---
+++ index.ts
@@ -0,0 +1,12 @@
+import { type PluginMeta } from "@/hook";
+
+export const PLUGINS: PluginMeta = {
+    nx:          "com.system.epaper",
+    name:        "epaper",
+    version:     "1.0.0",
+    description: "epaper with dynamic box selection.",
+    author:      "System",
+    path:        "https://github.com/HOTLancerX/epaper.git",
+    icon:        "streamline-plump:news-paper",
+    color:       "from-violet-500 to-purple-600",
+};
